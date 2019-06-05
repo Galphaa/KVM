881a91a116ecc458b1203d88a32d9f40f0d3cf84
Giorgi Keratishvili <gkeratishvili@gmail.com>
2019-06-05 17:48:55 +0400

    Update centos.sh

--- centos.sh
+++ centos.sh
@@ -1,2 +1,3 @@
 yum -y install libvirt qemu-kvm virt-install virt-top libguestfs-tools bridge-utils
 yum groupinstall virtualization-client virtualization-platform virtualization-tools	
+yum install wget openssl-devel  python-sphinx gcc make python-devel openssl-devel kernel-devel graphviz kernel-debug-devel autoconf automake rpm-build redhat-rpm-config libtool python-twisted-core python-zope-interface PyQt4 desktop-file-utils libcap-ng-devel groff checkpolicy selinux-policy-devel -y
